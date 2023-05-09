d929ab2da21f4cdf3c494cc6fee689f15ec699d1
Radhika-Lele <radhika.khaparde@gmail.com>
2023-05-09 14:10:00 -0400

    fetch API and use state and create Recipe component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useEffect , useState} from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 function App() {
-  return (
+  const appId = '7785af4a';
+  const appKey = '6a527e9912e5a0858be1a719e797c88c';
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect( () =>{
+      let url = `https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`;
+      fetch(url)
+        .then( (response) => {
+           return response.json();
+        }).then( (data) =>{
+            setRecipes(data.hits)
+             console.log(data.hits);
+        })
+        
+        
+      // getRecipes();
+  }, []);
+
+  //  const getRecipes = async ()=>{
+  //     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`);
+  //     const data = await response.json();
+  //     console.log(data);
+  //  }
+   
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form className="searchForm">
+        <input className="searchBar" type="text "></input>
+        <button className="searchButton" type="submit">Search</button>
+       </form>
+       {recipes.map( (recipe) => (
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        />
+       ))}
     </div>
-  );
+  )
+  
 }
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,10 @@
+function Recipe( {title, calories, image} ){
+    return(
+        <div>
+            <h1>{title}</h1>
+            <p>{calories}</p>
+            <img src={image} alt="" />
+        </div>
+    )
+}
+export default Recipe;
